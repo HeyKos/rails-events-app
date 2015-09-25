2bbcb84bf4bdad211754fca59280d375b623406a
heykos <mkoser@andculture.com>
2015-09-25 15:12:06 -0400

    Added new event view and action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to '/events'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :location, :occurs_on)
+    end
+
+    def event_find
+      Event.find(params[:id])
+    end
 end
