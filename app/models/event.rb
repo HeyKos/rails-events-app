3306fcaa9deb721cc4315824b59ee60fdc7e1a45
heykos <mkoser@andculture.com>
2015-09-28 14:02:01 -0400

    Added custom validation and after create callback to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,18 +18,38 @@
 #
 
 class Event < ActiveRecord::Base
+  # Associations
   belongs_to :user
   has_many :registrations
   has_many :attendees, :through => :registrations, :source => :user
   has_and_belongs_to_many :categories
+  # Validation
   validates :title, presence: true
   validates :description, presence: true
   validates :occurs_on, presence: true
   validates :url, presence: true
   validates :location, presence: true
+  validate :has_not_occurred
+  # Event callbacks
+   after_create :ensure_owner_attends
 
   def long_title
     "#{title} - #{location} - #{occurs_on}"
   end
+
+  def is_in_the_past?
+    occurs_on < Date.today
+  end
+
+  protected
+    def has_not_occurred
+      errors.add("occurs_on", "is in the past") if occurs_on && is_in_the_past?
+    end
+
+    def ensure_owner_attends
+      unless attendees.include? user
+        attendees << user
+      end
+    end
 
 end
